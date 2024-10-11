24d0dd9ac2b5a2fa818c522adc5dc69acf3daa08
davidelettieri <davide.lettieri@gmail.com>
2024-10-11 21:28:04 +0000

    minor fixes

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,5 +28,5 @@ dotnet tool run ClangSharpPInvokeGenerator @GenerateTreeSitterJson.rsp
 dotnet tool run ClangSharpPInvokeGenerator @GenerateTreeSitterRuby.rsp
 dotnet tool run ClangSharpPInvokeGenerator @GenerateTreeSitterHtml.rsp
 dotnet tool run ClangSharpPInvokeGenerator @GenerateTreeSitterEmbeddedTemplate.rsp
-dotnet build ./src/TreeSitter.Bindings
-dotnet test --no-build ./tests/TreeSitter.Bindings.UnitTests
+dotnet build
+dotnet test --no-build
