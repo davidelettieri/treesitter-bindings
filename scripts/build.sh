82d42b2c1460629b7360c4c6262972a540148baf
davidelettieri <davide.lettieri@gmail.com>
2024-10-11 21:14:56 +0000

    remove split projects

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,15 +18,15 @@ cd tree-sitter-ruby
 make clean
 make
 cd ..
-cp tree-sitter/libtree-sitter.so.0 tree-sitter-packages/libtreesitter.runtime.linux-x64/libtree-sitter.so
-cp tree-sitter-json/libtree-sitter-json.so.0.0 tree-sitter-packages/libtreesitter.runtime.linux-x64/libtree-sitter-json.so
-cp tree-sitter-html/libtree-sitter-html.so.0.0 tree-sitter-packages/libtreesitter.runtime.linux-x64/libtree-sitter-html.so
-cp tree-sitter-embedded-template/libtree-sitter-embedded-template.so.0.0 tree-sitter-packages/libtreesitter.runtime.linux-x64/libtree-sitter-embedded-template.so
-cp tree-sitter-ruby/libtree-sitter-ruby.so.0.0 tree-sitter-packages/libtreesitter.runtime.linux-x64/libtree-sitter-ruby.so
+cp tree-sitter/libtree-sitter.so.0 packages-definitions/libtreesitter.runtime.linux-x64/libtree-sitter.so
+cp tree-sitter-json/libtree-sitter-json.so packages-definitions/libtreesitter.runtime.linux-x64/libtree-sitter-json.so
+cp tree-sitter-html/libtree-sitter-html.so packages-definitions/libtreesitter.runtime.linux-x64/libtree-sitter-html.so
+cp tree-sitter-embedded-template/libtree-sitter-embedded_template.so packages-definitions/libtreesitter.runtime.linux-x64/libtree-sitter-embedded-template.so
+cp tree-sitter-ruby/libtree-sitter-ruby.so packages-definitions/libtreesitter.runtime.linux-x64/libtree-sitter-ruby.so
 dotnet tool run ClangSharpPInvokeGenerator @GenerateTreeSitter.rsp
 dotnet tool run ClangSharpPInvokeGenerator @GenerateTreeSitterJson.rsp
 dotnet tool run ClangSharpPInvokeGenerator @GenerateTreeSitterRuby.rsp
 dotnet tool run ClangSharpPInvokeGenerator @GenerateTreeSitterHtml.rsp
 dotnet tool run ClangSharpPInvokeGenerator @GenerateTreeSitterEmbeddedTemplate.rsp
-dotnet build
-dotnet test --no-build
+dotnet build ./src/TreeSitter.Bindings
+dotnet test --no-build ./tests/TreeSitter.Bindings.UnitTests
--- scripts/download-tree-sitter-compiled-artifacts.sh
+++
@@ -1,8 +0,0 @@
-version=$1
-
-allArchs=('linux-arm' 'linux-arm64' 'linux-x64' 'linux-x86' 'windows-arm64' 'windows-x64' 'windows-x86')
-
-for a in ${allArchs[@]}; do
-  echo https://github.com/tree-sitter/tree-sitter/releases/download/v0.23.0/tree-sitter-${a}.gz
-done
-
